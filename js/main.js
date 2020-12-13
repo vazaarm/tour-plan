902c0a7605a19f433c8a5e32b812de39b3900944
vazaarm <vgaribian@mail.ru>
2020-12-13 16:34:47 +0300

    creat: php request

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -12,7 +13,7 @@ var hotelSlider = new Swiper('.hotel-slider', {
           enabled: true,
           onlyInViewport: false,
         }
-})
+});
 
 var reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
@@ -28,12 +29,31 @@ var reviewsSlider = new Swiper('.reviews-slider', {
           enabled: true,
           onlyInViewport: false,
         }
-})
+});
 
-var menuButton = document.querySelector(".menu-button");
+var menuButton = $(".menu-button");
 var noScroll = document.querySelector("body");
-menuButton.addEventListener("click", function() {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+menuButton.on("click", function() {
+  $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   noScroll.classList.toggle("scroll-hidden");
+});
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal)
+    
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
